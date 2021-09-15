b21f46716db9a0e9b573185cfc0602e33d341fc9
Arya Tomar <sagartomar.269@gmail.com>
2021-09-16 00:12:51 +0530

    JavaScript Exercise

---
+++ Java Script/JavaScript_Exe1.js
@@ -0,0 +1,7 @@
+// JavaScript Exercise
+alert("Pounds to Kgs Converter")
+var a=prompt("Enter Weight in Pounds:")
+var b=parseInt(a)
+var c=b*0.454
+alert(a + " Pounds= "+ c+" kgs")
+console.log("Conversion Completed")
