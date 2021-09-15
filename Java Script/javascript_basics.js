056da41be24561219b04b4e123ea8361019b70db
Arya Tomar <sagartomar.269@gmail.com>
2021-09-15 23:16:15 +0530

    In this section we will learn basics of JavaScript

---
+++ Java Script/javascript_basics.js
@@ -0,0 +1,5 @@
+// We can run JavaScript directly into the browser console, or as a full.js scrpit connected to an HTML file. JavaScript is a full programming language, meaning unlike HTML or CSS it's support things such as arrays, loops, and general logics.
+alert("Welcome to Your Bank")
+var deposite=prompt("How much would you like to deposite today?")
+alert("You have deposited:" + deposite)
+console.log("Your account is credited with "+ deposite)
