171e78c8eb872f875895c2504600d8459d0d26cd
Arya Tomar <sagartomar.269@gmail.com>
2021-09-21 18:23:05 +0530

    JS array basics

---
+++ Java Script/JavaScript_Array.js
@@ -0,0 +1,19 @@
+var country1="USA"
+var country2="Germany"
+var country3="China"
+var countries=["USA","China","Germany"]
+console.log("indexing");
+console.log(countries);
+console.log(countries[1]);
+
+var myArr=["One","Two","Three"]
+console.log(myArr);
+console.log("Using pop")
+console.log(myArr.pop())
+
+var arr=["A","B","C"]
+for(letter of arr){
+  console.log(letter)
+}
+console.log("forEach() method")
+arr.forEach(alert);
