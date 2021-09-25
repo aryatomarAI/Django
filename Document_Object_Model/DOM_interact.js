8d637530771b802d6382c31a5b6a25fc206985ea
Arya Tomar <sagartomar.269@gmail.com>
2021-09-25 13:35:41 +0530

    Document Object Model interaction basics

---
+++ Document_Object_Model/DOM_interact.js
@@ -0,0 +1,9 @@
+var x = document.querySelector("p")
+x.textContent
+console.log(" ");
+console.log("Replacing Paragraph text with a new text")
+x.textContent="This is the new text that we are using, this is possible with the help of textContent attribute. "
+
+x.innerHTML="This is <strong> BOLD </strong>"
+
+var special=document.querySelector("#special")
