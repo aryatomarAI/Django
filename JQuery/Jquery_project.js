737e070fcc48e5809523379d7540792d00b9f34f
Arya Tomar <sagartomar.269@gmail.com>
2021-09-28 18:08:44 +0530

    Jquery Project- Connecting Four!

---
+++ JQuery/Jquery_project.js
@@ -0,0 +1,4 @@
+var player1=prompt("Please Enter your Name Player1, Your chip color will be blue: ")
+var player1Color='#003049'
+var player2=prompt("Please Enter your Name Player2, Your chip color will be red: ")
+var player2Color="#d62828"
