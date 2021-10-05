ad742c8b79218764b47aefaa013e7106b71435ae
Arya Tomar <sagartomar.269@gmail.com>
2021-10-05 23:28:34 +0530

    Learning Static files concept in Django

--- Django_level_one/my_first_project/first_app/views.py
+++ Django_level_one/my_first_project/first_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    my_dict={'insert_me':"Hello, Now i am inside First_app/index.html"}
+    my_dict={'insert_me':"In this blog we will see how Django makes everthing simple for us just by few commands. In this blog we are using Static files concept of Django"}
     return render(request,'first_app/index.html',context=my_dict)
