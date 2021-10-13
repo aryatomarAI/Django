b1a522448b091038db3aa29e7dd7a618a8daa0e3
Arya Tomar <sagartomar.269@gmail.com>
2021-10-13 15:29:31 +0530

    Form Valiodation

--- Django_level_two/basicforms/formApp/forms.py
+++ Django_level_two/basicforms/formApp/forms.py
@@ -1,6 +1,26 @@
 from django import forms
+# Buit-in Bot Cathcher
+from django.core import validators
+
+
+# Custom validators
+def check_for_Capital(value):
+    if value[0].islower():
+        raise forms.ValidationError("First Word Should be in Capitals")
+
+
 
 class Form_name(forms.Form):
-    name=forms.CharField()
+    name=forms.CharField(validators=[check_for_Capital])
     email=forms.EmailField()
     text=forms.CharField(widget=forms.Textarea)
+    botcatcher=forms.CharField(required=False,widget=forms.HiddenInput,validators=[validators.MaxLengthValidator(0)])
+
+# Function defined for botcatcher
+#    def clean_botcatcher(self):
+    #    botcatcher=self.cleaned_data["botcatcher"]
+
+    #    if len(botcatcher) > 0:
+    #        raise forms.ValidationError("Alert BOT!!!!")
+
+    #    return botcatcher
