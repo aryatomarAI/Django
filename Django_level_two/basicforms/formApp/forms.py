d24388472a6f0e8705389ebfae62e7755eb10a21
Arya Tomar <sagartomar.269@gmail.com>
2021-10-13 15:57:38 +0530

    Basics of Form Validation

--- Django_level_two/basicforms/formApp/forms.py
+++ Django_level_two/basicforms/formApp/forms.py
@@ -13,9 +13,19 @@ def check_for_Capital(value):
 class Form_name(forms.Form):
     name=forms.CharField(validators=[check_for_Capital])
     email=forms.EmailField()
+    verify_email=forms.EmailField(label="Please enter your email again:")
     text=forms.CharField(widget=forms.Textarea)
+
     botcatcher=forms.CharField(required=False,widget=forms.HiddenInput,validators=[validators.MaxLengthValidator(0)])
 
+
+    def clean(self):
+        all_clean_data=super().clean()
+        email=all_clean_data["email"]
+        vmail=all_clean_data["verify_email"]
+
+        if vmail != email:
+            raise forms.ValidationError("Your Email is not matches")
 # Function defined for botcatcher
 #    def clean_botcatcher(self):
     #    botcatcher=self.cleaned_data["botcatcher"]
