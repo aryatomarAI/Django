a72277db8404d6e29379e021b690b3347bce5f03
Arya Tomar <sagartomar.269@gmail.com>
2021-10-18 20:23:03 +0530

    Making changes in our project for User Authentication

--- Django_level_four/UserPro/UserApp/models.py
+++ Django_level_four/UserPro/UserApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserProfileInfo(models.Model):
+    # create relationship
+    user=models.OneToOneField(User,on_delete=models.CASCADE,)
+
+    # Portfolio site
+    portfolio=models.URLField(blank=True)
+
+    # profile pic
+    profile_pic=models.ImageField(upload_to="profile_pics",blank=True)
+
+    def __str__(self):
+        return self.user.username
