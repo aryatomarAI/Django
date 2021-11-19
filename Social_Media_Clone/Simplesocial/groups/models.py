d537031d2f7801c4ce2ca42d273a560542859e87
Arya Tomar <sagartomar.269@gmail.com>
2021-11-19 23:57:26 +0530

    Creating Models for posts app

--- Social_Media_Clone/Simplesocial/groups/models.py
+++ Social_Media_Clone/Simplesocial/groups/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.utils.text import slugify
+import misaka
 # Create your models here.
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+
+
+from django import template
+
+register = template.Library()
+
+class Group(models.Model):
+    pass
+
+
+class GroupMember(models.Model):
+    group=models.ForeignKey(Group,on_delete=models.CASCADE,related_name="memberships")
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="user_groups")
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together=("group","user")
