097da800380de41eb44c7de1aad7303bcb2c9a8d
Arya Tomar <sagartomar.269@gmail.com>
2021-11-21 17:12:49 +0530

    creating urls for groups app

--- Social_Media_Clone/Simplesocial/groups/admin.py
+++ Social_Media_Clone/Simplesocial/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class GroupMemberInline(admin.TabularInline):
+    model=modes.GroupMember
+
+
+admin.site.register(models.Group)
