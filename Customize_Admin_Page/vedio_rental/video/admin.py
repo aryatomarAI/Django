d224f1167b8d7b336add59603a9dfa129e77d2ac
Arya Tomar <sagartomar.269@gmail.com>
2022-01-10 00:11:25 +0530

    Customize admin page

--- Customize_Admin_Page/vedio_rental/video/admin.py
+++ Customize_Admin_Page/vedio_rental/video/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from . import models
+
+class MovieAdmin(admin.ModelAdmin):
+    fields=['release_year','title','length']
+    search_fields=['title','release_year']
+    list_filter=['release_year']
+    list_display=['release_year','length','title']
+    list_editable=['length']
+
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie,MovieAdmin)
